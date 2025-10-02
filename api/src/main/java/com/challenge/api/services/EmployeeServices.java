70e7a4da1a752f30d7c2bdb37f780ca4b9f7df08
bobromero <rromero123102@gmail.com>
2025-10-02 14:53:46 -0400

    added service layer

---
+++ api/src/main/java/com/challenge/api/services/EmployeeServices.java
@@ -0,0 +1,14 @@
+package main.java.com.challenge.api.services;
+
+import com.challenge.api.model.Employee;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeeServices {
+    public List<Employee> getAllEmployees() {
+        List<Employee> employees = new ArrayList<Employee>();
+        System.out.println("getting information from database");
+        Employee emp1;
+        return employees;
+    }
+}
