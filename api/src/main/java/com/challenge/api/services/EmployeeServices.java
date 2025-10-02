f1011ce14285d0309e10ff95ab031b270075d46a
bobromero <rromero123102@gmail.com>
2025-10-02 18:05:41 -0400

    first two services added

--- api/src/main/java/com/challenge/api/services/EmployeeServices.java
+++ api/src/main/java/com/challenge/api/services/EmployeeServices.java
@@ -1,14 +1,24 @@
 package main.java.com.challenge.api.services;
 
 import com.challenge.api.model.Employee;
+
+import main.java.com.challenge.data.EmployeeData;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class EmployeeServices {
     public List<Employee> getAllEmployees() {
-        List<Employee> employees = new ArrayList<Employee>();
-        System.out.println("getting information from database");
-        Employee emp1;
-        return employees;
+        return Employee.EMPLOYEES; // would be sql statment querying the database
+    }
+    public Employee getEmployeeByUuid(UUID uuid) {
+        List<Employee> employees = Employee.EMPLOYEES;// sql would be Select from where uuid = uuid
+        Employee ret = null;
+        for (Employee employee : employees) {
+            if (employee.getUuid() == uuid) {
+                ret = employee;
+            }
+        }
+        return ret;
     }
 }
