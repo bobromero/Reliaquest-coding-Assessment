ed96f770e877e4601bee242e54607816ac16c687
bobromero <rromero123102@gmail.com>
2025-10-03 12:20:48 -0400

    updated for get employee by uuid

--- api/src/main/java/com/challenge/api/controller/EmployeeController.java
+++ api/src/main/java/com/challenge/api/controller/EmployeeController.java
@@ -1,12 +1,16 @@
 package com.challenge.api.controller;
 
 import com.challenge.api.model.Employee;
+import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
 import main.java.com.challenge.api.services.EmployeeServices;
+import main.java.com.challenge.data.EmployeeInfo;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -33,12 +37,15 @@ public class EmployeeController {
      * @return Requested Employee if exists
      */
     @GetMapping("/{uuid}")
-    public Employee getEmployeeByUuid(UUID uuid) {
+    public Employee getEmployeeByUuid(@PathVariable UUID uuid) {
         if (uuid == null) {
+            System.out.println("uuid is null");
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
+        System.out.println(uuid);
         Employee result = service.getEmployeeByUuid(uuid);
         if (result == null) {
+            System.out.println("result is null");
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
         return result;
@@ -51,6 +58,18 @@ public class EmployeeController {
      */
     @PostMapping
     public Employee createEmployee(@RequestBody Object requestBody) {
+        EmployeeInfo empinfo = new EmployeeInfo.Builder()
+                .Age(37)
+                        .ContractHireDate(Instant.ofEpochSecond(1758433448))
+                        .ContractTerminationDate(null)
+                        .Email("TheDon@mail.com")
+                        .FirstName("Donald")
+                        .FullName("Donald Forrest")
+                        .JobTitle("Sales Representative")
+                        .LastName("Forrest")
+                        .Salary(270000)
+                        .Uuid(UUID.randomUUID())
+                        .build();
         throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED);
     }
 }
